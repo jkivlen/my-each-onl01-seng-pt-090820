f945d9086403e34adcfbf6f79ceed8a51e492211
Jimmy Kivlen <jimmykivlen@gmail.com>
2020-09-20 03:29:18 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,6 +1,10 @@
 def my_each(collection) # put argument(s) here
   # code here
-  while 
-  yield(collection)
+  i = 0
+  while i < collection.length
+  yield(collection[i])
+  i = i+1
+  end
+  collection
   
 end
